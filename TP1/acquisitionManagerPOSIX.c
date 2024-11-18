f2dfc9b573fb1c4aa75bfd12f0c97433d6fdf114
BOUZIANE Nadhir <bouzianenadirus@gmail.com>
2024-11-18 19:24:34 +0100

    Implemented acquisitionPOSIX.c

--- TP1/acquisitionManagerPOSIX.c
+++ TP1/acquisitionManagerPOSIX.c
@@ -11,8 +11,15 @@
 #include "iAcquisitionManager.h"
 #include "debug.h"
 
+#define BUFFER_SIZE 4
 
-//producer count storage
+// Storage buffer
+MSG_BLOCK buffer[BUFFER_SIZE];
+
+unsigned int writeIndex = 0; // Index where the producer will write
+unsigned int readIndex = 0;	 // Index where the consumer will read
+
+// producer count storage
 volatile unsigned int produceCount = 0;
 
 pthread_t producers[4];
@@ -20,60 +27,113 @@ pthread_t producers[4];
 static void *produce(void *params);
 
 /**
-* Semaphores and Mutex
-*/
-//TODO
+ * Semaphores and Mutex
+ */
+// mutexes
+pthread_mutex_t bufferLock;
+
+// Semaphores
+sem_t buffEmpty; // semaphore to indicate the number of empty spots in the buffer
+sem_t buffFull;	 // semaphore to indicate the number of full spots in the buffer
 
 /*
-* Creates the synchronization elements.
-* @return ERROR_SUCCESS if the init is ok, ERROR_INIT otherwise
-*/
+ * Creates the synchronization elements.
+ * @return ERROR_SUCCESS if the init is ok, ERROR_INIT otherwise
+ */
 static unsigned int createSynchronizationObjects(void);
 
 /*
-* Increments the produce count.
-*/
+ * Increments the produce count.
+ */
 static void incrementProducedCount(void);
 
 static unsigned int createSynchronizationObjects(void)
 {
 
-	//TODO
+	if (sem_init(&buffEmpty, 0, BUFFER_SIZE) == -1 || sem_init(&buffFull, 0, 0) == -1)
+	{
+		perror("Semaphore creation failed");
+		return ERROR_INIT;
+	}
+	pthread_mutex_init(&bufferLock, NULL);
 	printf("[acquisitionManager]Semaphore created\n");
 	return ERROR_SUCCESS;
 }
 
 static void incrementProducedCount(void)
 {
-	//TODO
+	produceCount++;
 }
 
 unsigned int getProducedCount(void)
 {
 	unsigned int p = 0;
-	//TODO
+	p = produceCount;
 	return p;
 }
 
-MSG_BLOCK getMessage(void){
-	//TODO
+MSG_BLOCK getMessage(void)
+{
+	MSG_BLOCK message;
+	do
+	{
+		getInput(0, &message); // Get input for the message
+		if (messageCheck(&message) == 0)
+		{
+			printf("Message checksum failed\n");
+		}
+	} while (messageCheck(&message) == 0); // Repeat until the checksum is correct
+
+	return message;
 }
 
-//TODO create accessors to limit semaphore and mutex usage outside of this C module.
+// accessors to limit semaphore and mutex usage outside of this C module.
+void lockBuffer(void)
+{
+	pthread_mutex_lock(&bufferLock);
+}
+
+void unlockBuffer(void)
+{
+	pthread_mutex_unlock(&bufferLock);
+}
+
+void waitForEmptyBuffer(void)
+{
+	sem_wait(&buffEmpty);
+}
+
+void signalEmptyBuffer(void)
+{
+	sem_post(&buffEmpty);
+}
+
+void waitForFullBuffer(void)
+{
+	sem_wait(&buffFull);
+}
+
+void signalFullBuffer(void)
+{
+	sem_post(&buffFull);
+}
 
 unsigned int acquisitionManagerInit(void)
 {
 	unsigned int i;
 	printf("[acquisitionManager]Synchronization initialization in progress...\n");
-	fflush( stdout );
+	fflush(stdout);
 	if (createSynchronizationObjects() == ERROR_INIT)
 		return ERROR_INIT;
-	
+
 	printf("[acquisitionManager]Synchronization initialization done.\n");
 
 	for (i = 0; i < PRODUCER_COUNT; i++)
 	{
-		//TODO
+		if (pthread_create(&producers[i], NULL, &produce, NULL) != 0)
+		{
+			perror("Producer thread creation failed");
+		}
 	}
 
 	return ERROR_SUCCESS;
@@ -84,23 +144,48 @@ void acquisitionManagerJoin(void)
 	unsigned int i;
 	for (i = 0; i < PRODUCER_COUNT; i++)
 	{
-		//TODO
+		// join the producer threads
+		if (pthread_join(producers[i], NULL) != 0)
+		{
+			perror("Producer thread join failed");
+		}
 	}
 
-	//TODO
+	// destroy semaphores and mutex
+	printf("Deleting the semaphores and mutex\n");
+	sem_destroy(&buffEmpty);
+	sem_destroy(&buffFull);
+	pthread_mutex_destroy(&bufferLock);
+
 	printf("[acquisitionManager]Semaphore cleaned\n");
 }
 
-void *produce(void* params)
+void *produce(void *params)
 {
 	D(printf("[acquisitionManager]Producer created with id %d\n", gettid()));
 	unsigned int i = 0;
 	while (i < PRODUCER_LOOP_LIMIT)
 	{
 		i++;
-		sleep(PRODUCER_SLEEP_TIME+(rand() % 5));
-		//TODO
+		sleep(PRODUCER_SLEEP_TIME + (rand() % 5));
+		// get the message from iSensor after it has been checked
+		MSG_BLOCK receivedMessage = getMessage();
+		// wait for an empty slot in the buffer
+		waitForEmptyBuffer();
+		// Lock access to the buffer
+		lockBuffer();
+		// write the message in the buffer
+		buffer[writeIndex] = receivedMessage;
+		// increment the write index within wrap bounds
+		writeIndex = (writeIndex + 1) % BUFFER_SIZE;
+		// increment the produced count
+		incrementProducedCount();
+		// post semaphore to account for filled slot
+		signalFullBuffer();
+		// unlock the buffer
+		unlockBuffer();
 	}
+
 	printf("[acquisitionManager] %d termination\n", gettid());
-	//TODO
+	// TODO
 }
